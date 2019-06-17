35fc935c2fbd88bdf8e356115a6f8da818a84696
abbyzahne <abbyzahne@gmail.com>
2019-06-17 00:57:31 -0400

    button scrolls to specific section, style changes

--- app.js
+++ app.js
@@ -3,3 +3,24 @@ const name = document.querySelectorAll('#full-name path');
 for(let i = 0; i < name.length; i++) {
     console.log(`Letter ${i} is ${name[i].getTotalLength()}`); 
 }
+
+// Scrolls to about me section
+$("#about-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about-me").offset().top},
+        1000);
+});
+
+// Scrolls to projects section
+$("#projects-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#projects").offset().top},
+        1000);
+});
+
+// Scrolls to contact section
+$("#contact-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top},
+        1000);
+});

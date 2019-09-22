7aae1dd06c9cfb8fdc25fe37007378f9b3395355
abbyzahne <abbyzahne@gmail.com>
2019-09-22 00:40:14 -0400

    updated website

--- app.js
+++ app.js
@@ -4,23 +4,33 @@ for(let i = 0; i < name.length; i++) {
     console.log(`Letter ${i} is ${name[i].getTotalLength()}`); 
 }
 
+// Scrolls to home section
+$("#home-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#home").offset().top},
+        1000);
+});
+
 // Scrolls to about me section
 $("#about-btn").click(function() {
     $('html, body').animate({
-        scrollTop: $("#about-me").offset().top},
+        scrollTop: $("#about-me").offset().top - 100},
         1000);
 });
 
 // Scrolls to projects section
 $("#projects-btn").click(function() {
     $('html, body').animate({
-        scrollTop: $("#projects").offset().top},
+        scrollTop: $("#projects").offset().top - 100},
         1000);
 });
 
 // Scrolls to contact section
 $("#contact-btn").click(function() {
     $('html, body').animate({
-        scrollTop: $("#contact").offset().top},
+        scrollTop: $("#contact").offset().top - 100},
         1000);
 });
+
+
+
